2f3fa80ae8b65932832c240851691c87336e6b4f
anmolb-cogo <anmol.bansal@cogoport.com>
2023-02-18 17:12:02 +0530

    add ui

--- src/components/Auth/Add.js
+++ src/components/Auth/Add.js
@@ -2,11 +2,27 @@ import React, { Component } from "react";
 import { Editor } from "react-draft-wysiwyg";
 import "../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./Auth.css";
+import ".././Main/Layout.css";
+import Input from "../minor/Input";
+import Select from "../minor/Select";
 
 function Add() {
   return (
-    <div className="article">
-      <Editor />
+    <div className="home">
+      <div className="main">
+        <span className="small">Add Blog</span>
+        <span className="big">Some Title</span>
+        <span className="filters">
+          <Input
+            type="text"
+            placeholder="Enter Article Title"
+            label="Article Title"></Input>
+          <Select label="Category"></Select>
+        </span>
+      </div>
+      <div className="article">
+        <Editor />
+      </div>
     </div>
   );
 }
